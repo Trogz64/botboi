4c5180b03cc22057c6e86e24162bf41f86b713f3
Barney <byoung03@qub.ac.uk>
2019-03-06 19:51:04 +0000

    Add token and login

--- src/main/java/com/github/barney/botboi/Main.java
+++ src/main/java/com/github/barney/botboi/Main.java
@@ -8,9 +8,12 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		String token = "your token";
+		String token = "NDE2NDA2NDg3MDI0NDAyNDMy.DuQ1zw.LCfFiK4uneZ6z9Odb3jL4ddGNTw";
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        System.out.println("\nLogged in as " + api.getClientId());
 	}
+	
+
 
 }
