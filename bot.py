98178af2c30b126bc67599e53932c89d0577285e
Barney <barneyoung@gmail.com>
2020-06-02 14:27:29 +0100

    Add plural to meep league reaction

--- bot.py
+++ bot.py
@@ -234,7 +234,7 @@ async def on_message(message):
                 if sent != True:        #after the for, if it is not found then it will just send the emoji as a message instead
                         await client.send_message(message.channel, msg)
 
-        if "MEEP" in message.content.upper().translate(str.maketrans('', '', string.punctuation)).split():
+        if "MEEP" in message.content.upper().translate(str.maketrans('', '', string.punctuation)).split() or "MEEPS" in message.content.upper().translate(str.maketrans('', '', string.punctuation)).split():
                 if message.author == client.user:
                         return
                 msg = "<:sexymeep:717366022574702696>"
